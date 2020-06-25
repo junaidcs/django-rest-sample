58161fdfff5ef37a0b5abd2bb201da9064f8bc1c
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-25 19:04:50 +0500

    paradigm serializer added

--- languages/serializers.py
+++ languages/serializers.py
@@ -6,3 +6,9 @@ class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
         fields = ('id', 'url', 'name', 'paradigm', 'desc')
+
+
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'url', 'name')
