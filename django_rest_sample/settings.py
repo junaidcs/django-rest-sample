36e2fca563ba309358b700de7fa33ed16dda1642
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-26 21:13:40 +0500

    permission on all routes

--- django_rest_sample/settings.py
+++ django_rest_sample/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # 'APPEND_SLASH': False,
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',) 
+}
