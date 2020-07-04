ef70ce252cc680c1c44958713cf0b9020fa7b014
Muhammad Junaid <m.junaidcs@gmail.com>
2020-07-04 21:44:25 +0500

    set decorators middleware

--- languages/decorators.py
+++ languages/decorators.py
@@ -8,12 +8,8 @@ def check_group_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         print(request.user.is_authenticated)
         print(request.user)
-        header = get_authorization_header(request)
-        print(get_user_model())
-        # header = request.headers
-        # print(header)
-        # token = request.get_raw_token(header)
-        # print(token)
+        # header = get_authorization_header(request)
+        # print(get_user_model())
         return view_func(request, *args, **kwargs)
 
     return wrapper_func
